7747541f160146ffd52c57913db0f4e596adbbbf
gprossliner <6724584+gprossliner@users.noreply.github.com>
2022-09-15 15:49:43 +0200

    ingressReferences are added the RouterInstance

--- controllers/ingress_controller.go
+++ controllers/ingress_controller.go
@@ -18,12 +18,17 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/go-logr/logr"
 	kasicov1 "github.com/world-direct/kasico/api/v1"
 )
 
@@ -32,6 +37,8 @@ type IngressReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
+
+const ingressFinalizer = "kasico.world-direct.at/ingressFinalizer"
 
 //+kubebuilder:rbac:groups=kasico.world-direct.at,resources=ingresses,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=kasico.world-direct.at,resources=ingresses/status,verbs=get;update;patch
@@ -56,7 +63,140 @@ func (r *IngressReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 	// to resolve this, this controller just inserts a reference to itself into the routerinstance.status.ingresses field.
 	// this is reconciliated later on, with debouncing
 
+	// https://sdk.operatorframework.io/docs/building-operators/golang/advanced-topics/#handle-cleanup-on-deletion
+
+	// Fetch the Ingress
+	ingress := &kasicov1.Ingress{}
+	err := r.Get(ctx, req.NamespacedName, ingress)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			log.Info("Ingress resource not found. Ignoring since object must be deleted.")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get Ingress")
+		return ctrl.Result{}, err
+	}
+
+	// Check if the Memcached instance is marked to be deleted, which is
+	// indicated by the deletion timestamp being set.
+	isMarkedToBeDeleted := ingress.GetDeletionTimestamp() != nil
+	if isMarkedToBeDeleted {
+		if controllerutil.ContainsFinalizer(ingress, ingressFinalizer) {
+			// Run finalization logic for memcachedFinalizer. If the
+			// finalization logic fails, don't remove the finalizer so
+			// that we can retry during the next reconciliation.
+			if err := r.finalizeIngress(ctx, log, ingress); err != nil {
+				return ctrl.Result{}, err
+			}
+
+			// Remove memcachedFinalizer. Once all finalizers have been
+			// removed, the object will be deleted.
+			controllerutil.RemoveFinalizer(ingress, ingressFinalizer)
+			err := r.Update(ctx, ingress)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+		return ctrl.Result{}, nil
+	}
+
+	// Add finalizer for this CR
+	if !controllerutil.ContainsFinalizer(ingress, ingressFinalizer) {
+		controllerutil.AddFinalizer(ingress, ingressFinalizer)
+		err = r.Update(ctx, ingress)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	err = r.registerIngress(ctx, log, ingress)
+	if err != nil {
+		log.Error(err, "Unable to register Ingress to the RouterInstance")
+		return ctrl.Result{}, err
+	}
+
 	return ctrl.Result{}, nil
+}
+
+func (r *IngressReconciler) registerIngress(ctx context.Context, log logr.Logger, ingress *kasicov1.Ingress) error {
+	log.Info("Check ingress registration on routerInstance")
+	routerInstance, err := r.getRouterInstance(ctx, log, ingress)
+
+	if err != nil {
+		return err
+	}
+
+	// check if this ingress is already known to the RouterInstance
+	index := findInstanceInIngresses(routerInstance.Status.Ingresses, ingress)
+	if index == -1 {
+		// add it
+		index = len(routerInstance.Status.Ingresses)
+		routerInstance.Status.Ingresses = append(routerInstance.Status.Ingresses, kasicov1.IngressReference{
+			Namespace: ingress.Namespace,
+			Name:      ingress.Name,
+		})
+	}
+
+	// update timestamp
+	routerInstance.Status.Ingresses[index].ReconciliationTime.Time = time.Now()
+	err = r.Status().Update(ctx, routerInstance)
+
+	return err
+}
+
+func (r *IngressReconciler) finalizeIngress(ctx context.Context, log logr.Logger, ingress *kasicov1.Ingress) error {
+	routerInstance, err := r.getRouterInstance(ctx, log, ingress)
+
+	if err != nil {
+		return err
+	}
+	index := findInstanceInIngresses(routerInstance.Status.Ingresses, ingress)
+
+	if index != -1 {
+		routerInstance.Status.Ingresses = removeIngressReferenceFromSlice(routerInstance.Status.Ingresses, index)
+
+		err = r.Status().Update(ctx, routerInstance)
+		return err
+	}
+
+	return nil
+}
+
+func removeIngressReferenceFromSlice(ingresses []kasicov1.IngressReference, index int) []kasicov1.IngressReference {
+	return append(ingresses[:index], ingresses[index+1:]...)
+}
+
+// findInstanceInIngresses returns the index of the ingress in the reference array, or -1 if not found
+func findInstanceInIngresses(ingresses []kasicov1.IngressReference, ingress *kasicov1.Ingress) int {
+
+	for i, ir := range ingresses {
+		if ir.Namespace == ingress.Namespace && ir.Name == ingress.Name {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func (r *IngressReconciler) getRouterInstance(ctx context.Context, log logr.Logger, ingress *kasicov1.Ingress) (*kasicov1.RouterInstance, error) {
+
+	list := &kasicov1.RouterInstanceList{}
+	err := r.Client.List(ctx, list)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, routerInstance := range list.Items {
+		if routerInstance.Spec.IngressClassName == ingress.Spec.IngressClassName {
+			return &routerInstance, nil
+		}
+	}
+
+	return nil, fmt.Errorf("Unable to find a RouterInstance with ingressClassName='%s'", ingress.Spec.IngressClassName)
 }
 
 // SetupWithManager sets up the controller with the Manager.
---
+++ controllers/ingress_controller_test.go
@@ -0,0 +1,53 @@
+package controllers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	kasicov1 "github.com/world-direct/kasico/api/v1"
+)
+
+func TestRemoveFirst(t *testing.T) {
+	ingresses := []kasicov1.IngressReference{
+		kasicov1.IngressReference{Namespace: "default", Name: "name1"},
+		kasicov1.IngressReference{Namespace: "default", Name: "name2"},
+		kasicov1.IngressReference{Namespace: "default", Name: "name3"},
+		kasicov1.IngressReference{Namespace: "default", Name: "name4"},
+	}
+
+	res := removeIngressReferenceFromSlice(ingresses, 0)
+	assert.Equal(t, 3, len(res))
+	assert.Equal(t, "name2", res[0].Name)
+	assert.Equal(t, "name3", res[1].Name)
+	assert.Equal(t, "name4", res[2].Name)
+}
+
+func TestRemoveLast(t *testing.T) {
+	ingresses := []kasicov1.IngressReference{
+		kasicov1.IngressReference{Namespace: "default", Name: "name1"},
+		kasicov1.IngressReference{Namespace: "default", Name: "name2"},
+		kasicov1.IngressReference{Namespace: "default", Name: "name3"},
+		kasicov1.IngressReference{Namespace: "default", Name: "name4"},
+	}
+
+	res := removeIngressReferenceFromSlice(ingresses, 3)
+	assert.Equal(t, 3, len(res))
+	assert.Equal(t, "name1", res[0].Name)
+	assert.Equal(t, "name2", res[1].Name)
+	assert.Equal(t, "name3", res[2].Name)
+}
+
+func TestRemoveMiddle(t *testing.T) {
+	ingresses := []kasicov1.IngressReference{
+		kasicov1.IngressReference{Namespace: "default", Name: "name1"},
+		kasicov1.IngressReference{Namespace: "default", Name: "name2"},
+		kasicov1.IngressReference{Namespace: "default", Name: "name3"},
+		kasicov1.IngressReference{Namespace: "default", Name: "name4"},
+	}
+
+	res := removeIngressReferenceFromSlice(ingresses, 1)
+	assert.Equal(t, 3, len(res))
+	assert.Equal(t, "name1", res[0].Name)
+	assert.Equal(t, "name3", res[1].Name)
+	assert.Equal(t, "name4", res[2].Name)
+}
